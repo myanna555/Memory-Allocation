7a5dae8fc1a020943387f6c5c8e5dbe4e5076af8
collosic <christian.collosi@gmail.com>
2015-05-11 10:01:26 -0700

    added some needed files from the book.  TODO: create make file and compile.

---
+++ memlib.h
@@ -0,0 +1,29 @@
+#ifndef MEMLIB
+#define MEMLIB value
+
+#include <stdlib.h>
+
+void *mem_init(int size);
+void *mem_sbrk(int incr);
+
+
+/***************************************************
+ * Wrappers for dynamic storage allocation functions
+ ***************************************************/
+
+void *Malloc(size_t size);
+void *Realloc(void *ptr, size_t size); 
+void *Calloc(size_t nmemb, size_t size); 
+void Free(void *ptr);
+
+
+/*
+ *  * Maximum heap size in bytes
+ *   */
+#define MAX_HEAP (20*(1<<20))  /* 20 MB */
+
+/* error handling */
+void unix_error(char *msg);
+
+
+#endif
